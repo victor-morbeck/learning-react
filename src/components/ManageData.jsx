c9f502f335718393f94f8d1c724e0ab5fc38d83d
Victor_Morbeck <victormorbeck212@gmail.com>
2023-07-18 20:59:37 -0300

    First Commit, project initialization

---
+++ src/components/ManageData.jsx
@@ -0,0 +1,20 @@
+
+import {useState} from 'react';
+const ManageData = () => {
+
+const someData = 12;
+const [number, setNumber] = useState(someData)
+
+  return (
+    <div>ManageData
+        <p>valor : {someData}</p>
+        <button onClick={()=> someData = 25}>Mudar Variável</button>
+        <div>
+            <p>Valor : {number}</p>
+            <button onClick={()=>setNumber(25)}>Mudar variavel com hooks</button>
+        </div>
+    </div>
+  )
+}
+
+export default ManageData
